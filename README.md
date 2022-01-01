672a61c6bf4fa2ab4e05006e9467cab0e976aa67
Ben Saufley <bensaufley+git@gmail.com>
2021-12-31 21:07:40 -0500

    Initial commit
    It runs! no tests, not a very good word library. But it works!

---
+++ README.md
@@ -0,0 +1,5 @@
+# wordle CLI
+
+A simple command-line implementation of Wordle, with five-letter word dictionary pulled from the open-source [Wordset Dictionary]
+
+[wordset dictionary]: https://github.com/wordset/wordset-dictionary
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bensaufley/wordle/internal/game"
+	"github.com/bensaufley/wordle/internal/input"
+	"github.com/pkg/term"
+)
+
+func main() {
+
+	// Check for TTY
+	t, err := term.Open("/dev/tty")
+	if err != nil {
+		panic(err)
+	}
+	t.Close()
+
+	for {
+		g := game.New()
+
+		g.Play()
+
+		if g.Won() {
+			fmt.Println("You won! 🎉")
+		} else {
+			fmt.Printf("Too bad. 😢 Want to know the word? (Yn) ")
+			if input.Confirm(true) {
+				fmt.Println(g.Word)
+			}
+		}
+
+		fmt.Printf("Would you like to play again? (yN) ")
+		if input.Confirm(false) {
+			fmt.Printf("\n")
+		} else {
+			fmt.Printf("\n")
+			break
+		}
+	}
+}
